d76b3018d3b6f871aae4bf095cde2839e8ec556c
Renata Deodório <renatadeod@gmail.com>
2022-11-14 09:19:00 -0300

    Operações Matemáticas

---
+++ operacoes.php
@@ -0,0 +1,12 @@
+<?php
+
+$idade = 21;
+$idadeDaqui10Anos = $idade +10 ;
+echo  $idadeDaqui10Anos;
+
+$soma = 2 + 2;
+$subtracao = 2 - 2;
+$multiplicacao = 2 * 2;
+$divisao = 2 / 2;
+$doissAoCubo = 2 ** 3; //(exponenciação)
+$restoDaDivisao = 10 % 3;
