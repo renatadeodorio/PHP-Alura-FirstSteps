34b99f23cc033e95a8d85db97511ce1da23c8f11
Renata Deodório <renatadeod@gmail.com>
2022-11-14 09:42:56 -0300

    Tipos de dados variável armazena

---
+++ tipo-de-dados.php
@@ -0,0 +1,10 @@
+<?php
+
+$idade = 21; //integer
+$salario = 1000.301; //double ou float ou decimal
+$divisao = 10 / 3; //double ou float ou decimal
+$texto = "Ola mundo"; //string
+$verdadeiro = true; //boolean
+$falso = false; //boolean
+
+//echo gettype($idade);
