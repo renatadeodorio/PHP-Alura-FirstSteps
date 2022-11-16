6307c28ceb55aef19f443a40f10c4468a1f78866
Renata Deodório <renatadeod@gmail.com>
2022-11-16 15:58:20 -0300

    Tomando decisóes if/else

--- decisoes.php
+++ decisoes.php
@@ -1,11 +1,15 @@
 <?php
 
-$idade = 17;
+$idade = 16;
 
 echo "Você só pode entrar se tiver a partir de 18 anos" . PHP_EOL;
 
 if ($idade >= 18) {
     echo "Você tem $idade anos." . PHP_EOL;
     echo "Pode entrar";
+} else {
+    echo "Você só tem $idade anos. Você não pode entrar";
 }
-echo "Você só tem $idade anos. Você não pode entrar";
+
+echo PHP_EOL;
+echo "Adeus!";
